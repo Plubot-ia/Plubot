74a0601857cf826a3108e84c0a96f2112d98fe33
Sebastian Redigonda <sebastianredigonda@Mac-mini-de-Sebastian.local>
2025-03-31 01:49:50 -0300

    Cancelamos Celery por ahora

--- celery_config.py
+++ celery_config.py
@@ -1,10 +1,12 @@
 from celery import Celery
+import os
 
 def make_celery(app):
+    redis_url = os.getenv('redis://red-cvl1ohre5dus73bv2ha0:6379', 'redis://localhost:6379/0')  # Fallback para local
     celery = Celery(
         app.import_name,
-        backend='redis://localhost:6379/0',
-        broker='redis://localhost:6379/0'
+        backend=redis_url,
+        broker=redis_url
     )
     celery.conf.update(app.config)
     return celery
@@ -12,3 +14,6 @@ def make_celery(app):
 def init_celery(app):
     celery = make_celery(app)
     return celery
+
+
+
